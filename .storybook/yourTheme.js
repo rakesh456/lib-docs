37e1898bae6aaac6d1ade7fb09ef2da7760ac8db
neeta1995 <neetasawant.ns@gmail.com>
2020-03-10 12:19:06 +0530

    dp changes

---
+++ .storybook/yourTheme.js
@@ -0,0 +1,6 @@
+import { create } from '@storybook/theming';
+import logo from '../stories/assets/Capture.png';
+
+export default create({
+  brandImage: '../stories/assets/Capture.png',
+});
--- stories/componentStories/datePickerStories/datePicker.stories.js
+++ stories/componentStories/datePickerStories/datePicker.stories.js
@@ -27,7 +27,7 @@ import '../../style.css'
     "<body>",
     "<h2>Date Picker</h2>",
     " ",
-    '<date-picker id="datepicker" data-options=\'{"displayFormat": "DD/MM/YYYY", "iconAlignment":"left", "showErrorMessage": true, "dateStringAlignment": "left", "lowerLimit": "08/08/2015", "upperLimit": "30/09/2022", "validationMessages": [{"inValidFormat": "Invalid DOB"}, { "outsideRange": ""}] , "isDisabled": false, "showButtons": true, "dateButtonPrimary": "", "showClearIcon": false, "manualEntry": true, "disabledList": ["01/12/2019", "15/10/2020", "01/11/2020", "20/11/2019"], "indicatorList": [{ "dates": ["01/10/2019","02/11/2019"], "color": "#333" }, { "dates": ["02/09/2019","01/08/2019"], "color": "#ff0000" }]}\'></date-picker>',
+    '<date-picker id="datepicker" data-options=\'{"displayFormat": "DD/MM/YYYY", "iconAlignment":"left", "showErrorMessage": true, "dateStringAlignment": "left", "lowerLimit": "08/08/2015", "upperLimit": "30/09/2022", "validationMessages": [{"inValidFormat": "Invalid DOB"}, { "outsideRange": ""}] , "isDisabled": false, "showButtons": true, "dateButtonPrimary": "", "showClearIcon": false, "manualEntry": true, "disabledList": ["01/12/2019", "15/10/2020", "01/11/2020", "20/11/2019"], "indicatorList": [{ "dates": ["01/10/2019","02/11/2019"], "color": "#333" }, { "dates": ["02/09/2019","01/08/2019"], "color": "#ff0000" }],"calendarLocation": "down"}\'></date-picker>',
     " ",
     "<script src='bundle.js'></script>",
     "</body>",
