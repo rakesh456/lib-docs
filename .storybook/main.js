7ebb0ff6d9fbd9eab57b36d7a36390d402bd4472
neeta1995 <neetasawant.ns@gmail.com>
2020-03-24 15:52:51 +0530

    doc changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 
 module.exports = {
   stories: ['../stories/**/*.stories.(js|mdx)'],
-  addons: [ '@storybook/addon-links','@storybook/addon-docs','@storybook/addon-storysource','@storybook/preset-create-react-app'],
+  addons: [ '@storybook/addon-links','@storybook/addon-docs','@storybook/addon-storysource'],
 };
